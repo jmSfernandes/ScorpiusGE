5a9be72e691b82d5c6cbf3728800b56564461527
Marcelo Fernandes <jmslfernandes@gmail.com>
2022-06-27 11:08:33 +0100

    -- Added firebase_admin.json example; -- added SimpleMessage Object to deal with simple notifications;

--- Scorpius/EndpointDefinitions/FirebaseEndpointDefinition.cs
+++ Scorpius/EndpointDefinitions/FirebaseEndpointDefinition.cs
@@ -11,31 +11,30 @@ public class FirebaseEndpointDefinition : IEndpointDefinition
     {
         app.MapPost("/notify", SendNotification)
             .WithName("Notify FCM").WithTags("Notify FCM");
-        app.MapPost("/notifyMessage", SendNotification)
-            .WithName("Notify FCMMessage").WithTags("Notify FCM");
+        app.MapPost("/notifyMessage", SendNotificationMessage)
+            .WithName("Notify FCM Simple Message").WithTags("Notify FCM");
     }
 
     public void DefineServices(IServiceCollection services)
     {
         //services.AddScoped<EstimationsService>();
         services.AddScoped<IFirebaseService, FirebaseService>();
-        services.AddSingleton<INotificationParser,NotificationParser>();
+        services.AddSingleton<INotificationParser, NotificationParser>();
     }
 
     //use this if we want to use services instead of direct implementation
-    private async Task<IResult> SendNotification(IFirebaseService exampleService, Subscription sub)
+    private async Task<IResult> SendNotification(IFirebaseService firebaseService, Subscription sub)
     {
         //var id = body["id"]?.ToString();
-        var obj = (JObject) JObject.Parse(sub.Data[0].ToString());
-
-        return await exampleService.SendNotification(obj);
+        var obj = JObject.Parse(sub.Data[0].ToString()) ?? null;
+        if (obj == null)
+            return Results.Problem("The data array should not be empty");
+        
+        return await firebaseService.SendNotification(obj);
     }
 
-    private async Task<IResult> GetSleepData(IFirebaseService exampleService, JObject body)
+    private async Task<IResult> SendNotificationMessage(IFirebaseService exampleService, SimpleMessage simpleMessage)
     {
-        var id = body["id"]?.ToString();
-        var message = body["message"]?.ToString();
-
-        return await exampleService.SendNotificationMessage(id, message);
+        return await exampleService.SendNotificationMessage(simpleMessage.Topic, simpleMessage.Message);
     }
 }
---
+++ Scorpius/Models/SimpleMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace Scorpius.Models;
+
+public class SimpleMessage
+{
+    public string Topic { get; set; }
+    public string Message { get; set; }
+}
